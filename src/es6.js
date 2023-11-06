42613182d8b26af7246d3050e990ebaacd4ac854
Kirill <91202716+DON-BOLSHOF@users.noreply.github.com>
2023-11-06 13:45:21 +0500

    Done

--- src/es6.js
+++ src/es6.js
@@ -5,26 +5,59 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName(fio) {}
+function fioToName(fio) {
+    let arr = fio.split(" ");
+    arr.splice(2,1)
+    return arr.reverse().join(" ");
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique(array) {}
+function filterUnique(array) {
+    return [...new Set(array)];
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference(array) {}
+function calculateSalaryDifference(array) {
+    array.sort()
+    return array[array.length - 1] / array[0] || false;
+}
 
 // Реализуйте класс "словарь слов" (как толковый словарь)
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // Словарь - (string, string), и все это не null и не undefined
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+    constructor() {
+        this.dict = new Map();
+    };
+
+    _inputIsNotString(key, description) {
+        return typeof key !== "string" || typeof description !== "string"
+    }
+    set(key, description) {
+        if (this._inputIsNotString(key, description)) {
+            throw new Error('Invalid argument type');
+        }
+
+        this.dict.set(key, description);
+    }
+
+    get(key) {
+        var desription = this.dict.get(key);
+        if (!desription) {
+            throw new Error("This word is not in the dictionary")
+        }
+
+        return desription;
+    }
+}
 
 module.exports = {
     fioToName,
