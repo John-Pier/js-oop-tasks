315903be4e15972f0dd680d4700466cf43a06783
Sept1m-L <142082581+Sept1m-L@users.noreply.github.com>
2023-11-26 13:44:58 +0300

    test 3

--- src/es6.js
+++ src/es6.js
@@ -5,25 +5,48 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName(fio) {}
+function fioToName(fio) {
+    const [secondName, firstName] = fio.split(" ");
+    return `${firstName} ${secondName}`;
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique(array) {}
+function filterUnique(array) {
+    return [...new Set(array)];
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
-function calculateSalaryDifference(array) {}
+function calculateSalaryDifference(array) {
+    return array.length === 0 ? false : Math.max(...array) / Math.min(...array);
+}
 
 // Реализуйте класс "словарь слов" (как толковый словарь)
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // Словарь - (string, string), и все это не null и не undefined
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+    constructor() {
+        this.dict = new Map();
+    }
+    add(key, value) {
+        if (typeof (key) !== 'string' || typeof (value) !== 'string') {
+            console.log('Values must be string!');
+        }
+        else this.dict.set(key.toLowerCase(), value.toLowerCase());
+    }
+    getValue(key) {
+        if (typeof (key) !== 'string' || !this.dict.has(key.toLowerCase())) {
+            return "No key";
+        }
+        else return this.dict.get(key.toLowerCase());
+    }
+}
 
 module.exports = {
     fioToName,
