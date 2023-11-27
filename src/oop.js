b8f4f17c489674d9d9d2179f8bbdf2d30ab4ee56
Loner887 <100552193+Loner887@users.noreply.github.com>
2023-11-27 10:19:14 +0500

    Update oop.js

--- src/oop.js
+++ src/oop.js
@@ -3,27 +3,60 @@
  * Если координаты не переданы - 0,0; Аналогично если только 1 координата.
  * Реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
  */
-class Point {}
+class Point {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
 
+    distanceToOrigin() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2);
+    }
+}
 /**
  * Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
  * который будет наследоваться от точки в двумерном пространстве.
  * Реализовать статический метод, который возвращает расстояние между Point3D.
  */
 class Point3D extends Point {
-    static vectorLength(a, b) {}
-}
+    constructor(x = 0, y = 0, z = 0) {
+        super(x, y);
+        this.z = z;
+    }
 
+    static vectorLength(pointA, pointB) {
+        const dx = pointB.x - pointA.x;
+        const dy = pointB.y - pointA.y;
+        const dz = pointB.z - pointA.z;
+
+        return Math.sqrt(dx ** 2 + dy ** 2 + dz ** 2);
+    }
+}
 /**
  * Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала.
  * Предусмотреть 2 варианта инициализации - массивом в конструкторе (из него создается очередь) и без параметров.
  * Для тех, кто доверяет, но проверяет: написать тесты на методы класса (oop.spec.js)
  */
 class Queue {
-    push;
-    pop;
-    size;
-    clear;
+    constructor(items = []) {
+        this.items = items;
+    }
+
+    push(element) {
+        this.items.push(element);
+    }
+
+    pop() {
+        return this.items.shift();
+    }
+
+    get size() {
+        return this.items.length;
+    }
+
+    clear() {
+        this.items = [];
+    }
 }
 
 module.exports = {
