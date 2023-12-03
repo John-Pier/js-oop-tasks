62e44b45dd73d9a10849b428d8f38c0c0014edb2
BronzeHunter <121955265+BronzeHunter@users.noreply.github.com>
2023-12-04 01:42:01 +0500

    es6.js

--- src/es6.js
+++ src/es6.js
@@ -1,48 +1,34 @@
 "use strict";
 
-class Dictionary {
-  constructor() {
-    this.dictionary = new Map();
-  }
-
-  addWord(key, value) {
-    if (typeof key === 'string' && typeof value === 'string') {
-      this.dictionary.set(key, value);
-    } else {
-      throw new Error('Both key and value should be strings');
-    }
-  }
-
-  getDefinition(key) {
-    if (typeof key === 'string') {
-      return this.dictionary.get(key);
-    } else {
-      throw new Error('Key should be a string');
-    }
-  }
-}
-
 function fioToName(fio) {
-  const [lastName, firstName, middleName] = fio.split(' ').filter(Boolean);
-  return `${firstName} ${lastName}`;
+    const [lastName, firstName] = fio.split(' ');
+    return `${firstName} ${lastName}`;
 }
 
 function filterUnique(array) {
-  return Array.from(new Set(array));
+    return [...new Set(array)];
 }
 
 function calculateSalaryDifference(array) {
-  if (array.length === 0) {
-    return null; // or any falsy value as per the requirement
-  }
-  const maxSalary = Math.max(...array);
-  const minSalary = Math.min(...array);
-  return maxSalary / minSalary;
+    if (array.length === 0) {
+        return null; // or any other appropriate falsy value
+    }
+
+    const maxSalary = Math.max(...array);
+    const minSalary = Math.min(...array);
+
+    if (minSalary === 0) {
+        throw new Error("Minimum salary should not be zero");
+    }
+
+    return maxSalary / minSalary;
 }
 
+class Dictionary {}
+
 module.exports = {
-  fioToName,
-  filterUnique,
-  Dictionary,
-  calculateSalaryDifference
+    fioToName,
+    filterUnique,
+    Dictionary,
+    calculateSalaryDifference
 };
