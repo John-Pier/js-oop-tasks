b3f883b52ec369c277a1289f9f96b704d61e62e6
BronzeHunter <121955265+BronzeHunter@users.noreply.github.com>
2023-12-04 01:10:40 +0500

    es6.js

--- src/es6.js
+++ src/es6.js
@@ -1,33 +1,48 @@
 "use strict";
-// в данных задачах нужно использовать возможности es6
-// ко всем заданиям можно (а местами и нужно) дописать свои тесты в файле es6.spec.js
-// Можно менять параметры функций (например сделать им значения по умолчанию)
 
-// Напишите функцию, которая принимает ФИО пользователя и возвращает
-// строку формата Имя Фамилия
-function fioToName(fio) {}
+class Dictionary {
+  constructor() {
+    this.dictionary = new Map();
+  }
 
-// преобразуйте массив чисел так, чтобы в нем остались только
-// уникальные элементы
-// присмотритесь к коллекции "Set"
-function filterUnique(array) {}
+  addWord(key, value) {
+    if (typeof key === 'string' && typeof value === 'string') {
+      this.dictionary.set(key, value);
+    } else {
+      throw new Error('Both key and value should be strings');
+    }
+  }
 
-// Задача: разница зарплат
-// в функцию приходит массив из n зарплат сотрудников фирмы
-// ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
-// сотрудника превышает зарплату самого низкооплачиваемого
-function calculateSalaryDifference(array) {}
+  getDefinition(key) {
+    if (typeof key === 'string') {
+      return this.dictionary.get(key);
+    } else {
+      throw new Error('Key should be a string');
+    }
+  }
+}
 
-// Реализуйте класс "словарь слов" (как толковый словарь)
-// класс должен быть безопасным и работать только со словами
-// присмотритесь к коллекции "Map"
-// Словарь - (string, string), и все это не null и не undefined
-// * покройте класс тестами
-class Dictionary {}
+function fioToName(fio) {
+  const [lastName, firstName, middleName] = fio.split(' ').filter(Boolean);
+  return `${firstName} ${lastName}`;
+}
+
+function filterUnique(array) {
+  return Array.from(new Set(array));
+}
+
+function calculateSalaryDifference(array) {
+  if (array.length === 0) {
+    return null; // or any falsy value as per the requirement
+  }
+  const maxSalary = Math.max(...array);
+  const minSalary = Math.min(...array);
+  return maxSalary / minSalary;
+}
 
 module.exports = {
-    fioToName,
-    filterUnique,
-    Dictionary,
-    calculateSalaryDifference
+  fioToName,
+  filterUnique,
+  Dictionary,
+  calculateSalaryDifference
 };
