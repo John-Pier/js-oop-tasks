62971caae9175c304549f7bccfe31c5f86da3c75
YahiaNassar <146712983+YahiaNassar@users.noreply.github.com>
2023-12-05 13:58:39 +0500

    Update oop.js

--- src/oop.js
+++ src/oop.js
@@ -3,7 +3,16 @@
  * Если координаты не переданы - 0,0; Аналогично если только 1 координата.
  * Реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
  */
-class Point {}
+class Point {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    distanceToOrigin() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2);
+    }
+}
 
 /**
  * Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
@@ -11,7 +20,17 @@ class Point {}
  * Реализовать статический метод, который возвращает расстояние между Point3D.
  */
 class Point3D extends Point {
-    static vectorLength(a, b) {}
+    constructor(x = 0, y = 0, z = 0) {
+        super(x, y);
+        this.z = z;
+    }
+
+    static vectorLength(a, b) {
+        const deltaX = b.x - a.x;
+        const deltaY = b.y - a.y;
+        const deltaZ = b.z - a.z;
+        return Math.sqrt(deltaX ** 2 + deltaY ** 2 + deltaZ ** 2);
+    }
 }
 
 /**
@@ -20,10 +39,33 @@ class Point3D extends Point {
  * Для тех, кто доверяет, но проверяет: написать тесты на методы класса (oop.spec.js)
  */
 class Queue {
-    push;
-    pop;
-    size;
-    clear;
+    constructor(elements = []) {
+        this.queue = [...elements];
+    }
+
+    push(...element) {
+        if(element){
+            if(element.length > 1) {
+                element.forEach((e) => this.queue.push(e))
+            }else{
+                this.queue.push(element);
+            }
+        }else {
+            console.log("Element Can Not Be NULL")
+        }
+    }
+
+    pop() {
+        return this.queue.shift();
+    }
+
+    get size() {
+        return this.queue.length;
+    }
+
+    clear() {
+        this.queue = [];
+    }
 }
 
 module.exports = {
